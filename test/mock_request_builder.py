52d28a518b473148d6ea29f59ec149200e492604
Jared Weiss <jared@jaredweiss.net>
2014-04-21 17:38:54 -0400

    changed package path to work correctly

--- test/mock_request_builder.py
+++ test/mock_request_builder.py
@@ -3,9 +3,15 @@
 from gen_proto import check_grasp_reachability_pb2
 from gen_proto import run_recognition_pb2
 
+import roslib.packages
 
-def build_mock_check_grasp_reachability_request(filename="mock_check_grasp_reachability_requests/grasp_proto_5.saved_proto"):
-    f = open(filename, "rb")
+pkg_path = roslib.packages.get_pkg_dir('mock_graspit') + '/test/mock_check_grasp_reachability_requests/'
+
+
+
+
+def build_mock_check_grasp_reachability_request(filename="grasp_proto_5.saved_proto"):
+    f = open(pkg_path + filename, "rb")
     check_grasp_request = check_grasp_reachability_pb2.CheckGraspReachabilityRequest()
     check_grasp_request.ParseFromString(f.read())
     return check_grasp_request
